43f453c0e901aa83d525f69a1469ba831b03c6b8
MetalArend <metalarend@gmail.com>
2018-06-28 00:09:09 +0200

    enhance the tests and variables used

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ "${1:0:1}" = "-" ]; then
-  set -- /sbin/tini -- php /vendor/bin/parallel-lint "$@"
-elif [ "$1" = "/vendor/bin/parallel-lint" ]; then
+if [ "$(printf %c "$1")" = '-' ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/parallel-lint "$@"
+elif [ "$1" = "/composer/vendor/bin/parallel-lint" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "parallel-lint" ]; then
-  set -- /sbin/tini -- php /vendor/bin/"$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
 fi
 
 exec "$@"
